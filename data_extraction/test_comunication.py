8aae8f27aba01055d60813790a461ba926b6364e
Larissa Alves <51495537+lyalbat@users.noreply.github.com>
2024-01-04 00:00:49 -0300

    feat: add read and write communication

--- data_extraction/test_comunication.py
+++ data_extraction/test_comunication.py
@@ -1,3 +1,28 @@
 import serial
-#testing if commit is ok
-ser = serial.Serial('COM3', baudrate=9600, timeout=1)
+import time
+
+print("working ok")
+
+ser = serial.Serial('COM4', baudrate = 9600, timeout=1)
+time.sleep(3)
+numPoints = 4
+dataList = [0]*numPoints
+
+def getValues():
+    ser.write("r,-1".encode())
+    for i in range(0, numPoints):
+        dataList[i] = ser.readline().decode().split('\r\n')[0]
+    return dataList
+
+def changeValues(setpoint):
+   command = "w" + "," + str(setpoint)
+   ser.write(command.encode())
+
+while(1):
+    userInput = input('Would you like to monitor the system [m] or change set point [c] ? ')
+    if userInput == 'm':
+        data = getValues()
+        print(data)
+    elif userInput == 'c':
+        setpoint = input('What is your desired setpoint [use point notation. eg: 1.0] ? ')
+        changeValues(setpoint)
